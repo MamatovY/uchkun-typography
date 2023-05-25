8571b8b14b7004c4b4398add0c5d365ef030843a
MamatovY <mamatovyiman28@gmail.com>
2023-05-25 18:14:21 +0600

    fix scroll

--- js/main.js
+++ js/main.js
@@ -4,17 +4,30 @@ const header = document.querySelector('.header');
 
 burgerIcon.addEventListener('click', () => {
     header.classList.toggle('active');
+    toggleBodyOverflow(header)
 });
 const aside = document.querySelector('.aside');
 
 asideIcon.addEventListener('click', () => {
     aside.classList.toggle('active');
+    toggleBodyOverflow(aside)
 });
+
+function toggleBodyOverflow(component) {
+    const isActive = component.classList.contains('active');
+
+    if (isActive) {
+        document.body.style.overflow = 'hidden';
+    } else {
+        document.body.style.overflow = 'auto';
+    }
+}
 
 window.addEventListener('resize', () => {
     const screenWidth = window.innerWidth;
 
     if (screenWidth > 768) {
         header.classList.remove('active');
+        aside.classList.remove('active');
     }
 });
