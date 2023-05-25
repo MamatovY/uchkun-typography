e71a9f5d96128b6c74c19509e7def2c6bd4a7f24
MamatovY <mamatovyiman28@gmail.com>
2023-05-25 17:39:51 +0600

    finish 80%. TO DO aside scroll

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 const burgerIcon = document.querySelector('.header__burger-icon');
+const asideIcon = document.querySelector('.aside__button');
 const header = document.querySelector('.header');
 
 burgerIcon.addEventListener('click', () => {
     header.classList.toggle('active');
+});
+const aside = document.querySelector('.aside');
+
+asideIcon.addEventListener('click', () => {
+    aside.classList.toggle('active');
 });
 
 window.addEventListener('resize', () => {
