c3334284d5a2081bc3b4f3d03e4a2803df68739d
MamatovY <mamatovyiman28@gmail.com>
2023-05-26 17:18:07 +0600

    finish search

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 const burgerIcon = document.querySelector('.header__burger-icon');
 const asideIcon = document.querySelector('.aside__button');
 const header = document.querySelector('.header');
-const headerTop = document.querySelector('.header__top-lang'); // select the parent element
+const headerTop = document.querySelector('.header__top__lang'); // select the parent element
 const languages = headerTop.querySelectorAll('.header__top-en, .header__top-ru, .header__top-kg'); // select all language elements
+
+const search = document.querySelector('.header__top__search');
+const langs = document.querySelector('.header__top__lang');
+const searchClose = document.querySelector('.header__top-close');
+const langOpen = document.querySelector('.header__top-open');
+
+searchClose.addEventListener('click', e => {
+    e.preventDefault();
+    search.classList.remove('active');
+    langs.classList.add('active');
+});
+
+langOpen.addEventListener('click', e => {
+    e.preventDefault();
+    langs.classList.remove('active');
+    search.classList.add('active');
+});
 
 languages.forEach(language => {
     language.addEventListener('click', function () {
